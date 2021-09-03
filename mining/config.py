e6dcc9b8194cbadbb33d359b1eea95ceb1244c9f
Rubén <74114026+rubzip@users.noreply.github.com>
2021-09-04 01:25:59 +0200

    Delete config.py

--- mining/config.py
+++
@@ -1,5 +0,0 @@
-#API keys:
-consumer_key = "3AmHv9FiXMVVl4xmhvxmsFMhd"
-consumer_secret = "WkenfuzE64gADHIBEWfxrRyiMELI8Nlk10jgtqn7QS6FycxxDs"
-access_token_key = "1352956549786968064-9WOXuPdyuLUoE4fr1CC3S1bdFxYx1E"
-access_token_secret = "7OzroDfWtYsKjrhbyYZNmUILFdtCYURA1QfZ1Wpj97J5Z"
