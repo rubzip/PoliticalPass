9037e023a50cf503ac13e333c5748bf41b566fd3
Rubén <74114026+rubzip@users.noreply.github.com>
2022-06-07 13:38:13 +0200

    Update fun.py
    He separado la creacion de la API con la obtencion de Tweets

--- mining/fun.py
+++ mining/fun.py
@@ -2,26 +2,26 @@ import pandas as pd
 import tweepy
 from config import consumer_key, consumer_secret, access_token_key, access_token_secret
 
-#Takes at direction and returns 300 (by default) last tweets as a list
-#if there is some error, returns 0
-def import_tweets(at, number_tweets=300):
-#https://stackoverflow.com/questions/30359801/how-to-successfully-get-all-the-tweets-for-one-user-with-tweepy
+#This function initializes the API
+def init_api():
 	#API config:
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, secure=True)
 	auth.set_access_token(access_token_key, access_token_secret)
 
-	api = tweepy.API(auth)
+	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+	
+	return api
 
-	#tweets are stored in a list
-	tweets = []
-
+#Takes at direction and returns 300 (by default) last tweets as a list
+#if there is some error, returns 0
+def import_tweets(at, api, number_tweets=300):
+#https://stackoverflow.com/questions/30359801/how-to-successfully-get-all-the-tweets-for-one-user-with-tweepy
 	try:
-		raw_tweets = tweepy.Cursor(api.user_timeline,id=at[1:], tweet_mode="extended").items(number_tweets)
+		raw_tweets = tweepy.Cursor(api.user_timeline, id=at[1:], tweet_mode="extended").items(number_tweets)
 		for tweet in raw_tweets:
 			text_tweet = tweet.full_text
 			if not("RT @" in text_tweet):   #We exculde RTs
 				tweets.append(text_tweet.lower())
-
 		return(tweets)
 
 	except:
