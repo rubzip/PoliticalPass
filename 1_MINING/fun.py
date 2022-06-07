997f3ff39e08386288791939c2f1eb78b40adc9b
rubzip <rubgij@gmail.com>
2022-06-08 01:44:17 +0200

    Basta por hoy

--- 1_MINING/fun.py
+++ 1_MINING/fun.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import tweepy
+from datetime import datetime
+
+from sklearn.model_selection import train_test_split
 from config import consumer_key, consumer_secret, access_token_key, access_token_secret
 
 def init_api():
@@ -28,3 +31,33 @@ def import_tweets(account, api, number_tweets=300):
 	except:
 		return 0
 		
+
+def validadtion_split(X, val_size=0.2, random_state=666):
+	# Splits data in train/test and validation
+	X_t, X_val = train_test_split(X, val_size, random_state=random_state)
+
+	return X_t, X_val
+	
+
+def import_tweets_from_list(acc_list, n_of_tweets=300, max_time=3600):
+	api = init_api()
+	remaining = acc_list
+	all_tweets = dict()
+	
+	tic = datetime.now() 
+	toc = lambda: (datetime.now() - ini).total_seconds()
+	
+	while(remaining != [] and toc()<max_time):
+    	for acc in remaining:
+        	tweets_from_acc = import_tweets(acc, api, n_of_tweets)
+    
+        	if(tweets_from_acc == 0):
+            	print("ERROR: " + acc)
+        	else:
+            	all_tweets[acc] = tweets_from_acc
+            	remaining.remove(acc)
+    print('Elapsed in', toc()/60, 's')
+    
+    return all_tweets, remaining
+
+
