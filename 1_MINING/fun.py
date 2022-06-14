afe0e97f6192685e427dc1409300ec85b15d65d8
rubzip <rubgij@gmail.com>
2022-06-14 16:40:51 +0200

    I have improved data_mining.ipynb

--- 1_MINING/fun.py
+++ 1_MINING/fun.py
@@ -7,13 +7,34 @@ from config import consumer_key, consumer_secret, access_token_key, access_token
 
 def init_api():
 	#API config:
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret, secure=True)
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token_key, access_token_secret)
 
 	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 	
 	return api
 
+
+def validation_split(df, val_size=0.2, random_state=666):
+	# Splits data in train/test and validation
+	df_t, df_val = train_test_split(df, test_size=val_size, random_state=random_state)
+
+	return df_t.sort_index(), df_val.sort_index()
+
+def list_to_csv(tweets, classification, root):
+	data_to_csv = []
+	
+	accounts = tweets.keys()
+	for acc in accounts:
+		for tweet in tweets[acc]:
+			data_to_csv.append( (acc, tweet, classification[acc]) )
+
+	colunms = ['User', 'Tweet', 'Left']
+	df = pd.DataFrame(data_to_csv, columns=colunms)
+
+	df.to_csv(root, index=False)
+	
+	return df
 
 def import_tweets(account, api, number_tweets=300):
 	# This function scraps the last 300 tweets from account
@@ -30,34 +51,28 @@ def import_tweets(account, api, number_tweets=300):
 
 	except:
 		return 0
-		
-
-def validadtion_split(X, val_size=0.2, random_state=666):
-	# Splits data in train/test and validation
-	X_t, X_val = train_test_split(X, val_size, random_state=random_state)
-
-	return X_t, X_val
 	
 
-def import_tweets_from_list(acc_list, n_of_tweets=300, max_time=3600):
+def import_tweets_from_list(acc_list, n_of_tweets=300, max_time=3600, max_iter=10):
 	api = init_api()
 	remaining = acc_list
 	all_tweets = dict()
 	
 	tic = datetime.now() 
-	toc = lambda: (datetime.now() - ini).total_seconds()
+	toc = lambda: (datetime.now() - tic).total_seconds()
 	
-	while(remaining != [] and toc()<max_time):
-    	for acc in remaining:
-        	tweets_from_acc = import_tweets(acc, api, n_of_tweets)
-    
-        	if(tweets_from_acc == 0):
-            	print("ERROR: " + acc)
-        	else:
-            	all_tweets[acc] = tweets_from_acc
-            	remaining.remove(acc)
-    print('Elapsed in', toc()/60, 's')
-    
-    return all_tweets, remaining
+	i = 0
+	while(remaining != [] and toc()<max_time and i<max_iter):
+		i += 1
+		for acc in remaining:
+			tweets_from_acc = import_tweets(acc, api, n_of_tweets)
 
+			if(tweets_from_acc == 0):
+				print("ERROR: " + acc)
+			else:
+				all_tweets[acc] = tweets_from_acc
+				print('Completed: ' + acc)
+				remaining.remove(acc)
 
+	print('Elapsed in', toc()/60, 'min')
+	return all_tweets, remaining
